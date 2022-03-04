3028bbb77abb1dc90fdb0332f0b84132b5651a58
unknown <manvi19472@iiitd.ac.in>
2022-03-05 01:43:50 +0530

    A2: Q1:90 Q2: 90 Q3:0

--- 2019472_Assignment3/scripts/matlab/m_2019472_q1.m
+++ 2019472_Assignment3/scripts/matlab/m_2019472_q1.m
@@ -3,47 +3,53 @@ function [] = m_2019472_q1(imName)
 close all
 
 path = 'C:\Users\hp\Desktop\Manvi\Semesters\6th_WinterSemester\ComputerVision\ComputerVision22\2019472_Assignment3\';
+
+% The number of superpixels
 numSuperpixels = 500;
 
 Image = imread(strcat(path, 'inputs\', imName));
-[L, numLabels] = superpixels(Image, numSuperpixels);
-idx = label2idx(L);
 
-numRows = size(Image,1);
-numCols = size(Image,2);
-outputImage = zeros([numRows numCols]);
+% Calculate the superpixels
+[L, numLabels] = superpixels(Image, numSuperpixels);
+ids = label2idx(L);
+
+% Initialize matrices for storing datas
+nRows = size(Image,1);
+nCols = size(Image,2);
+salMap = zeros([nRows nCols]);
 meanColor = zeros([numLabels 3]);
 meanCoords = zeros([numLabels 2]);
 salValue = zeros([1 numLabels]);
 
 
-
-for labelVal = 1:numLabels
-    redIdx = idx{labelVal};
-    greenIdx = idx{labelVal}+numRows*numCols;
-    blueIdx = idx{labelVal}+2*numRows*numCols;
+% Calculate the mean colors and coordinates
+for label = 1:numLabels
+    meanColor(label, :) = [mean(Image(ids{label})) mean(Image(ids{label} + nRows*nCols)) mean(Image(ids{label} + 2*nRows*nCols))];       
     
-    meanColor(labelVal, :) = [mean(Image(redIdx)) mean(Image(greenIdx)) mean(Image(blueIdx))];   
-    
-    [row, col] = ind2sub(size(Image), idx{labelVal});
-    meanCoords(labelVal, :) = [mean(row), mean(col)];
+    [row, col] = ind2sub(size(Image), ids{label});
+    meanCoords(label, :) = [mean(row), mean(col)];
 end    
 
+
+% Calculate the saliency value for each superpixel
 for i = 1:numLabels
     for j = 1:numLabels
-        salValue(i) = salValue(i) + norm(meanColor(i, :) - meanColor(j, :))*(exp(-1*(norm(meanCoords(i, :) - meanCoords(j, :))/(numRows^2 + numCols^2)^0.5)));  
+        salValue(i) = salValue(i) + norm(meanColor(i, :) - meanColor(j, :))*(exp(-1*(norm(meanCoords(i, :) - meanCoords(j, :))/(nRows^2 + nCols^2)^0.5)));  
     end
 end
 
-for labelVal = 1:numLabels
-    labelIds = idx{labelVal};
-    outputImage(labelIds) = salValue(labelVal);
+% Make the saliency map
+for label = 1:numLabels
+    labelIds = ids{label};
+    salMap(labelIds) = salValue(label);
 end 
 
-outputImage = mat2gray(outputImage);
-imwrite(outputImage, strcat(path, 'outputs\', '2019472_Q1.jpg'), 'Quality', 100)
+% Normalize the saliency map and save the output
+salMap = mat2gray(salMap);
+imwrite(salMap, strcat(path, 'outputs\', '2019472_Q1.jpg'), 'Quality', 100)
 
+% Display the image
 figure
-imshow(outputImage)
+imshow(salMap)
 end
 
---
+++ 2019472_Assignment3/scripts/matlab/m_2019472_q2.m
@@ -0,0 +1,34 @@
+function [] = m_2019472_q2(imName)
+
+close all;
+path = 'C:\Users\hp\Desktop\Manvi\Semesters\6th_WinterSemester\ComputerVision\ComputerVision22\2019472_Assignment3\';
+
+Image = imread(strcat(path, 'inputs\', imName));
+[r, c, ~] = size(Image);
+
+% Turn to grayscale to get the coordinates
+gImage = rgb2gray(Image);
+[y, x] = find(gImage ~= 0); 
+coor = [x, y];
+
+
+featureImage = reshape(double(Image), [r*c, 3]);
+featureImage = cat(2, featureImage, coor);
+featureImage = normalize(featureImage);
+
+
+epsilons = [0.01, 0.02, 0.05, 0.08, 0.1, 0.12, 0.15, 0.2, 0.25, 0.3, 0.5, 1, 2, 10];
+minNumbers = [2, 5, 10, 15, 20, 25, 30, 40, 50, 80, 100];
+
+for i = 1:size(epsilons, 2)
+    for j = 1:size(minNumbers, 2)
+        ids = dbscan(featureImage, epsilons(i), minNumbers(j));
+        pixelLabels = reshape(ids, [r, c]);
+        pixelLabels = mat2gray(pixelLabels);
+        figure, imshow(pixelLabels)
+        imwrite(pixelLabels, strcat(path, 'outputs\2019472_Q2\', num2str(epsilons(i)), '-', num2str(minNumbers(j)), '.jpg'), 'Quality', 100)
+    end
+end
+
+
+end
